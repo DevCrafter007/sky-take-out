cf706fa6ff3eb93d07331ebeb0425d18c69c72b9
7274 <2645746107@qq.com>
2025-07-25 22:02:06 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.sky.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,34 +14,47 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "员工表实体类")
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty("主键值")
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("密码")
     private String password;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
+    @ApiModelProperty("账号状态")
     private Integer status;
 
     //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("创建员工信息时间")
     private LocalDateTime createTime;
 
     //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("更新员工信息时间")
     private LocalDateTime updateTime;
 
+    @ApiModelProperty("创建员工信息的id")
     private Long createUser;
 
+    @ApiModelProperty("更新员工信息的id")
     private Long updateUser;
 
 }
